ddac724515726f6052e5c646147b75c5543348a7
IsraelPina32 <israelpina047@gmail.com>
2025-11-07 18:31:48 -0300

    feat : adicionar novas logos das empresas e removendo as antigas

--- src/assets/images/companies/index.ts
+++ src/assets/images/companies/index.ts
@@ -1,9 +1,7 @@
 import enterpriseOne from "../../images/companies/enterpriseOne.svg";
 import enterpriseTwo from "../../images/companies/enterpriseTwo.svg";
-import enterpriseThree from "../../images/companies/enterpriseThree.svg";
+import enterpriseThree from "../../images/companies/enterpriseTree.svg";
 import enterpriseFour from "../../images/companies/enterpriseFour.svg";
-import enterpriseFive from "../../images/companies/enterpriseFive.svg";
-import enterpriseSix from "../../images/companies/enterpriseSix.svg";
 
 
 
@@ -12,12 +10,8 @@ export const companiesSlider = [
     enterpriseTwo,
     enterpriseThree,
     enterpriseFour,
-    enterpriseFive,
-    enterpriseSix,
     enterpriseOne,
     enterpriseTwo,
     enterpriseThree,
     enterpriseFour,
-    enterpriseFive,
-    enterpriseSix,
 ]
